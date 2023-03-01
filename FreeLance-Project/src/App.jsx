3beb0e56844690a167087041ade92c48c063edb3
James-Frown <21122@ctca.co.za>
2023-03-01 14:42:53 +0200

    Best Update Ever
    Leant javascript react props, and relabeled website to better name!! please try it out!!

--- FreeLance-Project/src/App.jsx
+++ FreeLance-Project/src/App.jsx
@@ -8,17 +8,17 @@ function App() {
       <div id="Hero__Section" className="App__Hero">
         <div className="Hero__Title__Wrapper">
           <h1  id="Hero__Title" className="Hero__Title">
-            Welcome to Free-Flix
+            Welcome to Free-4-Days
           </h1>
         </div>
         <div className="Hero__Subtitle__Wrapper">
           <h2  id="Hero__Subitle" className="Hero__Subitle">
-            This is a Netflix Clone
+            This is a Soap2day Clone
           </h2>
         </div>
         <div className="Hero__Description__Wrapper">
           <p  id="Hero__Description" className="Hero__Description">
-            Why pay for Netflix, when you can just stream here, for free!!
+            Why pay for Netflix or be bothered by ad's, when you can just stream here for free!!
           </p>
         </div>
       </div>
@@ -28,22 +28,8 @@ function App() {
           </h2>
       </div>
       <div id="Content__Section" className="App__Content">
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
-        <Flixcard />
+        <Flixcard Image="https://image.tmdb.org/t/p/w185/vSNxAJTlD0r02V9sPYpOjqDZXUK.jpg" Name="Captin America: The First Avenger" Director="Joe Johnston" Description="Steve Rogers, a rejected military soldier transforms into Captain America after taking a dose of a super-Soldier serum. But being Captain America comes at a price as he attempts to take down a war monger and a terrorist organization." Url="https://soap2day.works/frost-bite-2/" />
+        <Flixcard Image="https://image.tmdb.org/t/p/w185/gOnmaxHo0412UVr1QM5Nekv1xPi.jpg" Name="Cocain Bear" Director="Elizabeth Banks" Description="An oddball group of cops, criminals, tourists, and teens converge in a Georgia forest where a 500-pound black bear goes on a murderous rampage after unintentionally ingesting cocaine." Url="https://soap2day.works/cocaine-bear-jVfwCf/" />
       </div>
       <div id="Footer__Section" className="App__Footer">HI IM THE FOOTER</div>
     </div>
