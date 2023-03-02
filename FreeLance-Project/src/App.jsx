3507ba7d1b6e1531b4ee344cf8165bedf7af5895
James-Frown <21122@ctca.co.za>
2023-03-02 12:52:19 +0200

    Additional movie
    Adding one more movie

--- FreeLance-Project/src/App.jsx
+++ FreeLance-Project/src/App.jsx
@@ -30,6 +30,7 @@ function App() {
       <div id="Content__Section" className="App__Content">
         <Flixcard Image="https://image.tmdb.org/t/p/w185/vSNxAJTlD0r02V9sPYpOjqDZXUK.jpg" Name="Captin America: The First Avenger" Director="Joe Johnston" Description="Steve Rogers, a rejected military soldier transforms into Captain America after taking a dose of a super-Soldier serum. But being Captain America comes at a price as he attempts to take down a war monger and a terrorist organization." Url="https://soap2day.works/frost-bite-2/" />
         <Flixcard Image="https://image.tmdb.org/t/p/w185/gOnmaxHo0412UVr1QM5Nekv1xPi.jpg" Name="Cocain Bear" Director="Elizabeth Banks" Description="An oddball group of cops, criminals, tourists, and teens converge in a Georgia forest where a 500-pound black bear goes on a murderous rampage after unintentionally ingesting cocaine." Url="https://soap2day.works/cocaine-bear-jVfwCf/" />
+        <Flixcard Image="https://image.tmdb.org/t/p/w185/oBvKapnLAIrSSUYwMJvt3oQfT2u.jpg" Name="Expedition Mars" Director="Mark Davis" Description="Expedition Mars brings to life one of the greatest sagas of the Space Age, the epic adventures of Spirit and Opportunity, the rovers that saved NASA's Mars program after a string of failures in the 1990's." Url="https://soap2day.works/expedition-mars-9ijZtQ/" />
       </div>
       <div id="Footer__Section" className="App__Footer">HI IM THE FOOTER</div>
     </div>
